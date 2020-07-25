996842c2cd59f59bb46203daaf53f0bdd6d8a9a3
Charlene Glasgow <And2reaC@aol.com>
2020-07-24 23:01:27 -0400

    Building Framework

---
+++ app.py
@@ -0,0 +1,31 @@
+#  Import dependencies
+from flask import Flask, render_template
+from flask_pymongo import PyMongo
+import scraping
+
+# Set up Flask
+app = Flask(__name__)
+
+# Use flask_pymongo to set up mongo connection
+app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
+mongo = PyMongo(app)
+
+# Setup Flask routes: one fore viewing ansd one for scraping
+@app.route("/")
+def index():
+   mars = mongo.db.mars.find_one()
+   return render_template("index.html", mars=mars)
+
+@app.route("/scrape")
+def scrape():
+   mars = mongo.db.mars
+   mars_data = scraping.scrape_all()
+   mars.update({}, mars_data, upsert=True)
+   return "Scraping Successful!"
+
+# Update database
+.update(query_parameter, data, options)
+
+#  Run
+if __name__ == "__main__":
+   app.run()
